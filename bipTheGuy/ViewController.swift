7b539938592ce7457fd88f9daa9deec099f4b702
Joseph Marasco <marascoj@bc.edu>
2019-09-15 14:53:59 -0400

    Initial Commit

---
+++ bipTheGuy/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  bipTheGuy
+//
+//  Created by Joseph Marasco on 9/15/19.
+//  Copyright © 2019 Joseph Marasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageToPunch: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func libraryPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func imageTapped(_ sender: Any) {
+    }
+    
+}
+
